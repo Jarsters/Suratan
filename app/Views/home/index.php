f1edcb350da8aec91d213a3e271e6a8a42f798c4
Muhammad Farhan Ghaffar <fghafar80@gmail.com>
2021-07-11 20:09:32 +0700

    menambahkan navbar dan gambar pada page index

---
+++ app/Views/home/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Home</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <div class="col-1"></div>
+            <div class="col-10">
+                <div class="row">
+                    <div class="col-1">
+                        <a class="navbar-brand" href="#">
+                            <img src="D:\buat software bapak\Suratan\app\Views\assets\1200px-Coat_of_arms_of_Jakarta.svg.png" alt="" class="img-fluid">
+                        </a>                    
+                    </div> 
+                    <div class="col-5"></div>
+                    <div class="col text-center">
+                        <ul class="nav justify-content-end">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">
+                                    <img src="D:\buat software bapak\Suratan\app\Views\assets\icon_login2.png" alt="" class="img-fluid">
+                                    <h6>Login</h6>
+                                </a>
+                            </li>
+                        </ul>                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-1"></div>
+        </div>
+    </nav>
+    <hr/>
+    <img src="D:\buat software bapak\Suratan\app\Views\assets\Kantor_Wali_Kota_Jakarta_Timur.jpg" class="img-fluid" alt="">
+    <div class="container">
+        <div class="col-1"></div>
+        <div class="col-1">
+            <form class="form-inline">
+                <div class="form-group mx-sm-3 mb-2">
+                <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary mb-2">Confirm identity</button>
+            </form>
+        </div>
+        <div class="col-1"></div>
+    </div>
+</body>
+</html>
