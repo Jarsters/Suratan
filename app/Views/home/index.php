efc49a2b043af2d418acd983f42f5dd09854b287
Muhammad Farhan Ghaffar <fghafar80@gmail.com>
2021-07-11 22:07:49 +0700

    menambahkan search bar pada page index

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -8,45 +8,50 @@
     <title>Home</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <div class="col-1"></div>
-            <div class="col-10">
-                <div class="row">
-                    <div class="col-1">
-                        <a class="navbar-brand" href="#">
-                            <img src="D:\buat software bapak\Suratan\app\Views\assets\1200px-Coat_of_arms_of_Jakarta.svg.png" alt="" class="img-fluid">
-                        </a>                    
-                    </div> 
-                    <div class="col-5"></div>
-                    <div class="col text-center">
-                        <ul class="nav justify-content-end">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">
-                                    <img src="D:\buat software bapak\Suratan\app\Views\assets\icon_login2.png" alt="" class="img-fluid">
-                                    <h6>Login</h6>
-                                </a>
-                            </li>
-                        </ul>                        
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="col-1"></div>
+                <div class="col-10">
+                    <div class="row">
+                        <div class="col-1">
+                            <a class="navbar-brand" href="#">
+                                <img src="D:\buat software bapak\Suratan\app\Views\assets\static\1200px-Coat_of_arms_of_Jakarta.svg.png" alt="" class="img-fluid">
+                            </a>                    
+                        </div> 
+                        <div class="col-5"></div>
+                        <div class="col text-center">
+                            <ul class="nav justify-content-end">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">
+                                        <img src="D:\buat software bapak\Suratan\app\Views\assets\static\icon_login2.png" alt="" class="img-fluid">
+                                        <h6>Login</h6>
+                                    </a>
+                                </li>
+                            </ul>                        
+                        </div>
                     </div>
                 </div>
+                <div class="col-1"></div>
             </div>
-            <div class="col-1"></div>
-        </div>
-    </nav>
-    <hr/>
-    <img src="D:\buat software bapak\Suratan\app\Views\assets\Kantor_Wali_Kota_Jakarta_Timur.jpg" class="img-fluid" alt="">
-    <div class="container">
-        <div class="col-1"></div>
-        <div class="col-1">
+        </nav>
+    </header>
+    <div class="row">
+        <img src="D:\buat software bapak\Suratan\app\Views\assets\static\Kantor_Wali_Kota_Jakarta_Timur.jpg" class="img-fluid" alt="">
+    </div>
+    <br><br><br>
+    <div class="row justify-content-md-center">
+        <!-- <div class="col col-lg-2"></div> -->
+        <div class="col col-lg-6">
             <form class="form-inline">
                 <div class="form-group mx-sm-3 mb-2">
-                <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+                    <img src="D:\buat software bapak\Suratan\app\Views\assets\static\Search.png" alt="icon_search">
+                    <input type="password" class="form-control" id="inputPassword2" placeholder="Cari Berdasarkan Nomor Surat atau Tanggal">
                 </div>
-                <button type="submit" class="btn btn-primary mb-2">Confirm identity</button>
+                <button type="submit" class="btn btn-primary mb-2">Cari</button>
             </form>
         </div>
-        <div class="col-1"></div>
+        <!-- <div class="col col-lg-2"></div> -->
     </div>
 </body>
 </html>
