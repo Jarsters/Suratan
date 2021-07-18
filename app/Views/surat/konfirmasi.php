626176b240cdcb3b50101dead0997493034cfdce
Muhammad Farhan Ghaffar <fghafar80@gmail.com>
2021-07-18 22:25:53 +0700

    menambahkan texfield pada keterangan dan menambahkan kolom nomor surat

--- app/Views/surat/konfirmasi.php
+++ app/Views/surat/konfirmasi.php
@@ -12,28 +12,29 @@
                     <th scope="col" class="col col-2"><?= ($surat['no_surat']) ? 'Nomor Surat' : 'Nomor Disposisi' ?></th>
                     <th scope="col">Tanggal</th>
                     <th scope="col">Lokasi</th>
-                    <th scope="col" class="col col-2">Tanggal Surat</th>
+                    <th scope="col">Nomor Surat</th>
                     <th scope="col">Perihal</th>
+                    <th scope="col" class="col col-2">Tanggal Surat</th>
                     <th scope="col">Asal Surat</th>
                     <th scope="col">Keterangan</th>
                 </tr>
             </thead>
             <tbody>
                 <?php $length = sizeof(explode(',', $surat['kepada'])) ?>
-                <?php for($i = 0; $i < $length; $i++) : ?>
-                <tr>
-                    <td><?= ($surat['no_surat']) ? $surat['no_surat'] : $surat['no_disposisi'] ?></td>
-                    <td><?= explode(',', $surat['tanggal_surat'])[$i] ?></td>
-                    <td><?= explode(',', $surat['kepada'])[$i] ?></td>
-                    <td><?= date('m-d-Y', strtotime($surat['surat_dibuat'])) ?></td>
-                    <td><?= $surat['perihal'] ?></td>
-                    <td><?= $surat['asal_surat'] ?></td>
-                    <td><?= explode(',', $surat['keterangan'])[$i] ?></td>
-                </tr>
+                <?php for ($i = 0; $i < $length; $i++) : ?>
+                    <tr>
+                        <td><?= ($surat['no_surat']) ? $surat['no_surat'] : $surat['no_disposisi'] ?></td>
+                        <td><?= explode(',', $surat['tanggal_surat'])[$i] ?></td>
+                        <!-- tambahin buat nomor surat sa -->
+                        <td><?= $surat['perihal'] ?></td>
+                        <td><?= date('m-d-Y', strtotime($surat['surat_dibuat'])) ?></td>
+                        <td><?= $surat['asal_surat'] ?></td>
+                        <td><?= explode(',', $surat['keterangan'])[$i] ?></td>
+                    </tr>
                 <?php endfor; ?>
                 <tr>
                     <td>3</td>
-                    <td>Larry the Bird</td>
+                    <td>12/12/1212</td>
                     <td style="width: 15%;">
                         <select class="form-select" aria-label="Default select example">
                             <option selected>Pilih</option>
@@ -42,9 +43,11 @@
                             <option value="3">Three</option>
                         </select>
                     </td>
-                    <td>Thornton</td>
+                    <td>123/213/123/1</td>
+                    <td>coba perihal nya panjang banget</td>
                     <td>@fat</td>
-                    <td>Thornton</td>
+                    <td>SDN 011 Pagi duren sawit</td>
+                    <td><input type="text" class="form-control" placeholder="Masukkan keterangan ..." name="keterangan"></td>
                 </tr>
             </tbody>
         </table>
